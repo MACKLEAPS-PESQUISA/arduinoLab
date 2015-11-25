0d95a0af430f797499e8f4296f62adc8f7f4c95f
Gabriel Correia <gmfc@users.noreply.github.com>
2015-11-25 12:42:44 -0200

    Update alg.ino

--- blink/src/alg.ino
+++ blink/src/alg.ino
@@ -12,5 +12,10 @@ void loop() {
 
 double getC(double rpm, double rpmTarget, double rpmPerAmp = 336)
 {
+  // se dentro do dominio valido da funcao
+  if(rpm<rpmTarget){
     return (rpmTarget * log(rpm))/(rpmPerAmp * log(rpmTarget));
+  } else {
+    return 0;
+  }
 }
