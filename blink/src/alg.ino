ef0aa90fa784166e6f8c93cf899f43ab5cd53b76
Gabriel Correia <gmfc@users.noreply.github.com>
2016-01-29 15:40:58 -0200

    V2

--- blink/src/alg.ino
+++ blink/src/alg.ino
@@ -1,96 +1,168 @@
 /*
-  LiquidCrystal Library - Hello World
+====== LCD ======
+Pinagen:
+lcd(47, 46, 42, 43, 44, 45);
+lcd(RS, E, d4, d5, d6, d7);
 
- Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
- library works with all LCD displays that are compatible with the
- Hitachi HD44780 driver. There are many of them out there, and you
- can usually tell them by the 16-pin interface.
+-- Pinos usados
+* LCD RS pin -> (RS)
+* LCD Enable pin -> (E)
+* LCD D4 pin -> (d4)
+* LCD D5 pin -> (d5)
+* LCD D6 pin -> (d6)
+* LCD D7 pin -> (d7)
 
- This sketch prints "Hello World!" to the LCD
- and shows the time.
+-- Config. Pinos
+* LCD R/W pin -> ground
+* LCD VSS pin -> ground
+* LCD VCC pin -> 5V
+-- 10K resistor: (Ver se incluso na placa)
+* ends to +5V and ground
+* wiper to LCD VO pin (pin 3)
 
-  The circuit:
- * LCD RS pin to digital pin 12
- * LCD Enable pin to digital pin 11
- * LCD D4 pin to digital pin 5
- * LCD D5 pin to digital pin 4
- * LCD D6 pin to digital pin 3
- * LCD D7 pin to digital pin 2
- * LCD R/W pin to ground
- * LCD VSS pin to ground
- * LCD VCC pin to 5V
- * 10K resistor:
- * ends to +5V and ground
- * wiper to LCD VO pin (pin 3)
+-- Exemplo:
+http://www.arduino.cc/en/Tutorial/LiquidCrystal
+*/
+//#include <LiquidCrystal.h>
 
- Library originally added 18 Apr 2008
- by David A. Mellis
- library modified 5 Jul 2009
- by Limor Fried (http://www.ladyada.net)
- example added 9 Jul 2009
- by Tom Igoe
- modified 22 Nov 2010
- by Tom Igoe
+int RS = 47;
+int E = 46;
+int d4 = 42;
+int d5 = 43;
+int d6 = 44;
+int d7 = 45;
 
- This example code is in the public domain.
+//LiquidCrystal lcd(RS, E, d4, d5, d6, d7);
 
- http://www.arduino.cc/en/Tutorial/LiquidCrystal
- */
+/*
+====== CARRO ======
+Termos:
+RPM: Rotacao por minuto DA RODA DO CARRO
+A: Amperagem fornecida para o motor
 
-// include the library code:
-#include <LiquidCrystal.h>
+-- Constantes
+* maxRPM -> Rotacao maxima da roda (medicao)
+* rpmT -> Rotacao da roda desejada (a 25km/h) (Raio da roda sendo 24.5cm, o valor e obtido com a seguinte formula: 10.82*V sendo V a velocidade em Km/h)
+* RPMPA -> Variavel consedida pela analise do treinamento da RN
+* maxA -> Amperagem maxima aplicavel ao motor pelo driver
+*/
+double maxRPM = 5000;
+double rpmT = 281.32; // 26Km/h para roda com R=24.5cm
+double RPMPA = 336; // dados coletados em 16/10/15
+double maxA = 6;
 
-// initialize the library with the numbers of the interface pins
-LiquidCrystal lcd(47, 46, 42, 43, 44, 45);
+/*
+-- Variaveis (Calibraveis) --
 
-double rpm = 0;
-double rpmPerAmp = 336; // Calibre-me!
-double rpmTarget = 1600; // Velocidade desejada
+-- Velocidade
+* minI -> Leitura do velocimetro a 0 RPM (da roda)
+* maxI -> Leitura do velocimetro a @maxRPM (da roda)
+-- Amperagem
+* minO -> input do driver para a liberacao de 0 Amp do mesmo
+* maxO -> input do driver para a liberacao de @maxA Amp do mesmo
+*/
+double minI = 0;
+double maxI = 1022;
+double minO = 0;
+double maxO = 6; 
 
-int entradaRPMPin = A0;
-int entradaCorrentePin = A1; // Nao Implementado ainda
+/*
+-- Pinagem --
+* velo -> pino do velicimetro (input)
+* drive -> pino do driver (output)
+*/
+int velo = 0;
+int drive = 3;
 
-int saidaCorrentePin = 3;
-
-void setup() {
-  // Init LCD
-  lcd.begin(20, 4);
-  // Init out Pins
-  pinMode(saidaCorrentePin, OUTPUT);
+void setup()
+{
+  //lcd.begin(20, 4);
+  pinMode(drive, OUTPUT);
 }
 
-double algor(double rpm, double rpmTarget, double rpmPerAmp = 336)
+void loop()
+{
+	// Le RPM da roda
+	double x = rpmIn();
+	
+	// Normaliza Leitura
+	x = normIn(x);
+	
+	// Calcula amperagem a ser aplicada
+	x = algor(x);
+	
+	// Normaliza saida para o driver
+	x = normOut(x);
+	
+	// Envia para o driver
+	driv(x);
+}
+
+/*================= Metodos ==================*/
+
+/*
+	Le RPM da roda
+*/
+double rpmIn()
+{
+	return analogRead(velo);
+}
+
+/*
+	Normaliza Leitura de Entrada
+*/
+double normIn(double in)
+{
+	return (maxRPM*(in-minI))/(maxI-minI);
+}
+
+/*
+	Calcula amperagem a ser aplicada
+*/
+double algor(double in)
 {
   double result = 0;
   // se dentro do dominio valido da funcao
-  if(rpm<rpmTarget){
-    result = (rpmTarget * log(rpm))/(rpmPerAmp * log(rpmTarget));
+  if(in<rpmT){
+    result = (rpmT * log(in))/(RPMPA * log(rpmT));
   } else {
     result = 0;
   }
-
+  //lcdInfo(in,result);
   return result;
 }
 
-void loop() {
-  rpm = analogRead(entradaRPMPin);
-  // Mapeia a entrada. Calibrar!
-  rpm = map(rpm, 0,1022,0,2000);
-  
-  lcd.setCursor(0, 0);
-  lcd.print("RPM entrada");
-  lcd.setCursor(0, 1);
-  lcd.print(rpm);
-  lcd.setCursor(0, 2);
-  lcd.print("Saida A");
-  lcd.setCursor(0, 3);
-  double outpAmp = algor(rpm,rpmTarget,rpmPerAmp);
-  lcd.print(outpAmp);
-
-  outpAmp = map(outpAmp,0,5,0,1022);
-  
-  analogWrite(saidaCorrentePin, outpAmp);
-  
-  //lcd.print(algor(input,1600,336));
-  //input += 0.001;
+/*
+	Normaliza saida de para driver
+*/
+double normOut(double in)
+{
+	return ((maxO-minO)*in)/maxA
 }
+
+/*
+	Envia para driver
+*/
+void driv(double in)
+{
+	analogWrite(drive, in);
+}
+/*
+	LCD Info
+*/
+/*
+void lcdInfo(double rpm, double amp)
+{
+	lcd.setCursor(0, 0);
+	lcd.print("RPM da roda:");
+	
+	lcd.setCursor(0, 1);
+	lcd.print(rpm);
+	
+	lcd.setCursor(0, 2);
+	lcd.print("Aplicando Amps:");
+	
+	lcd.setCursor(0, 3);
+	lcd.print(amp);
+}
+*/
