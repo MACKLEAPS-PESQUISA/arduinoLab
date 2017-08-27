a9ceaecc27d80cdb8c0b87b82c485de7eecdb299
spoonm <spoonm@spoonm.org>
2017-08-26 23:47:32 -0300

    Constantes e comentários atualizados.
    Constantes devem seguir normas.

--- blink/src/alg.ino
+++ blink/src/alg.ino
@@ -22,7 +22,7 @@ lcd(RS, E, d4, d5, d6, d7);
 
 -- Exemplo:
 http://www.arduino.cc/en/Tutorial/LiquidCrystal
-*/
+
 //#include <LiquidCrystal.h>
 
 int RS = 47;
@@ -33,79 +33,76 @@ int d6 = 44;
 int d7 = 45;
 
 //LiquidCrystal lcd(RS, E, d4, d5, d6, d7);
+*/
 
 /*
-====== CARRO ======
-Termos:
-RPM: Rotacao por minuto DA RODA DO CARRO
-A: Amperagem fornecida para o motor
-
--- Constantes
-* maxRPM -> Rotacao maxima da roda (medicao)
-* rpmT -> Rotacao da roda desejada (a 25km/h) (Raio da roda sendo 24.5cm, o valor e obtido com a seguinte formula: 10.82*V sendo V a velocidade em Km/h)
-* RPMPA -> Variavel consedida pela analise do treinamento da RN
-* maxA -> Amperagem maxima aplicavel ao motor pelo driver
-*/
-double maxRPM = 5000;
-double rpmT = 281.32; // 26Km/h para roda com R=24.5cm
-double RPMPA = 336; // dados coletados em 16/10/15
-double maxA = 6;
-
-/*
--- Variaveis (Calibraveis) --
-
--- Velocidade
-* minI -> Leitura do velocimetro a 0 RPM (da roda)
-* maxI -> Leitura do velocimetro a @maxRPM (da roda)
--- Amperagem
-* minO -> input do driver para a liberacao de 0 Amp do mesmo
-* maxO -> input do driver para a liberacao de @maxA Amp do mesmo
-*/
-double minI = 0;
-double maxI = 1022;
-double minO = 0;
-double maxO = 6; 
+ * Dados específicos do carro.
+ *
+ * Aqui, unidades em rpm referem-se à velocidade angular da RODA DO CARRO.
+ * Unidades em A referem-se à amperagem fornecida para o MOTOR.
+ *
+ * RPM_MAX     :: velocidade angular máxima da roda
+ * RPM_TARGET  :: velocidade angular alvo
+ * RPM_PER_AMP :: velocidade angular obtida por cada ampere dado ao motor
+ * AMP_MAX     :: amperagem máxima aplicável ao motor pelo driver
+ *
+ * RPM_TARGET é obtido a partir da velocidade alvo e do raio da roda.
+ * RPM_PER_AMP foi coletado em 2015-10-16.
+ */
+const double RPM_MAX = 5000.0;
+const double RPM_TARGET = 281.32;
+const double RPM_PER_AMP = 336.0;
+const double AMP_MAX = 6;
 
 /*
--- Pinagem --
-* velo -> pino do velicimetro (input)
-* drive -> pino do driver (output)
-*/
-int velo = 0;
-int drive = 3;
+ * Entrada(velocidade angular em rpm) e saída(amperagem) da placa.
+ *
+ * INPUT_MIN  :: leitura do velocímetro a 0 rpm
+ * INPUT_MAX  :: leitura do velocímetro a RPM_MAX rpm
+ * OUTPUT_MIN :: saída para o driver liberar 0 A
+ * OUTPUT_MAX :: saída para o driver liberar AMP_MAX A
+ *
+ * Esses dados são importantes para a definição de intervalos de entrada e saída
+ * que o Arduino não consegue prever.
+ */
+const double INPUT_MIN = 0;
+const double INPUT_MAX = 1022;
+const double OUTPUT_MIN = 0;
+const double OUTPUT_MAX = 6;
+
+const int PIN_VELOCIMETRO = 0; // pino do velocímetro (entrada)
+const int PIN_DRIVER = 3;      // pino do driver (saída)
 
 void setup()
 {
   //lcd.begin(20, 4);
-  pinMode(drive, OUTPUT);
+  pinMode(PIN_DRIVER, OUTPUT);
 }
 
 void loop()
 {
 	// Le RPM da roda
 	double x = rpmIn();
-	
+
 	// Normaliza Leitura
 	x = normIn(x);
-	
+
 	// Calcula amperagem a ser aplicada
 	x = algor(x);
-	
+
 	// Normaliza saida para o driver
 	x = normOut(x);
-	
+
 	// Envia para o driver
 	driv(x);
 }
-
-/*================= Metodos ==================*/
 
 /*
 	Le RPM da roda
 */
 double rpmIn()
 {
-	return analogRead(velo);
+	return analogRead(PIN_VELOCIMETRO);
 }
 
 /*
@@ -113,7 +110,7 @@ double rpmIn()
 */
 double normIn(double in)
 {
-	return (maxRPM*(in-minI))/(maxI-minI);
+	return (RPM_MAX*(in-INPUT_MIN))/(INPUT_MAX-INPUT_MIN);
 }
 
 /*
@@ -123,8 +120,8 @@ double algor(double in)
 {
   double result = 0;
   // se dentro do dominio valido da funcao
-  if(in<rpmT){
-    result = (rpmT * log(in))/(RPMPA * log(rpmT));
+  if(in<RPM_TARGET){
+    result = (RPM_TARGET * log(in))/(RPM_PER_AMP * log(RPM_TARGET));
   } else {
     result = 0;
   }
@@ -137,7 +134,7 @@ double algor(double in)
 */
 double normOut(double in)
 {
-	return ((maxO-minO)*in)/maxA;
+	return ((OUTPUT_MAX-OUTPUT_MIN)*in)/AMP_MAX;
 }
 
 /*
@@ -145,7 +142,7 @@ double normOut(double in)
 */
 void driv(double in)
 {
-	analogWrite(drive, in);
+	analogWrite(PIN_DRIVER, in);
 }
 /*
 	LCD Info
@@ -155,13 +152,13 @@ void lcdInfo(double rpm, double amp)
 {
 	lcd.setCursor(0, 0);
 	lcd.print("RPM da roda:");
-	
+
 	lcd.setCursor(0, 1);
 	lcd.print(rpm);
-	
+
 	lcd.setCursor(0, 2);
 	lcd.print("Aplicando Amps:");
-	
+
 	lcd.setCursor(0, 3);
 	lcd.print(amp);
 }
